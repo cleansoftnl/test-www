3a13e80ff1e329af69c495b197d428bbaf73a126
Niels Drost <nielsdrost7@gmail.com>
2019-10-27 17:02:44 +0100

    wip

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,32 +1,5 @@
-const mix = require('laravel-mix');
-let tailwindcss = require('tailwindcss');
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
-
-const sassOptions = {
-    processCssUrls: false,
-    postCss: [tailwindcss('./tailwind.js')]
-};
-
-mix.js('resources/js/system/app.js', 'public/js/app.js')
-    .sourceMaps();
-mix.sass('resources/sass/system/app.scss', 'public/css/app.css')
-    .options(sassOptions);
-
-mix.js('resources/js/tenant/settings.js', 'public/js/settings.js')
-    .sourceMaps();
-mix.sass('resources/sass/tenant/settings.scss', 'public/css/settings.css')
-    .options(sassOptions);
-
-mix.js('resources/js/tenant/mailbox.js', 'public/js/mailbox.js')
-    .sourceMaps();
-mix.sass('resources/sass/tenant/mailbox.scss', 'public/css/mailbox.css')
-    .options(sassOptions);
+const mix = require("laravel-mix");
+mix.js("resources/js/app.js", "public/js").sass(
+    "resources/sass/app.scss",
+    "public/css"
+);
